c90adf274b8f89b595c6f0424f57466a090f1065
Tobias Hann <mail@tobiashann.de>
2020-03-10 16:24:26 +0100

    add notes and test

--- server.test.js
+++ server.test.js
@@ -1,6 +1,6 @@
 const getMatchingDrinks = require("./server");
 
-describe(getMatchingDrinks, async () => {
+describe(getMatchingDrinks, () => {
   it(" returns an empty array when ingredients are empty", async () => {
     const result1 = await getMatchingDrinks([]);
     expect(result1).toBe([]);
@@ -12,8 +12,44 @@ describe(getMatchingDrinks, async () => {
   });
 
   it(" returns the matching cocktails containing the ingredients", async () => {
-    const result3 = await getMatchingDrinks(["sugar", "lime juice"]);
+    const result3 = await getMatchingDrinks(["Sugar", "Lime juice"]);
     expect(result3).toEqual([
+      {
+        id: "11064",
+        name: "Banana Daiquiri",
+        ingredients: [
+          "Light rum",
+          "Triple sec",
+          "Banana",
+          "Lime juice",
+          "Sugar",
+          "Cherry"
+        ]
+      }
+    ]);
+  });
+
+  it(" is insensitive to the order of ingredients", async () => {
+    const result = await getMatchingDrinks(["lime juice", "sugar"]);
+    expect(result).toEqual([
+      {
+        id: "11064",
+        name: "Banana Daiquiri",
+        ingredients: [
+          "Light rum",
+          "Triple sec",
+          "Banana",
+          "Lime juice",
+          "Sugar",
+          "Cherry"
+        ]
+      }
+    ]);
+  });
+
+  it(" is insensitive to case of search teams", async () => {
+    const result = await getMatchingDrinks(["lime juice", "SUGAR"]);
+    expect(result).toEqual([
       {
         id: "11064",
         name: "Banana Daiquiri",
